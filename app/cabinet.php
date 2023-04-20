4e972f6f301c5d2c991a8a44b0f74e2ee7e047fa
patel-krishna <krishna.patel0621@gmail.com>
2023-04-20 18:32:05 -0400

    filtering of the cabinet in vanilla.js

--- app/cabinet.php
+++ app/cabinet.php
@@ -23,6 +23,33 @@
             padding: 20px;
             text-align: center;
             }
+
+            .filter-section {
+                display: flex;
+                flex-wrap: wrap;
+                }
+
+            .filter {
+                width: 50px;
+                height: 50px;
+                background-color: white;
+                margin: 5px;
+                width:fit-content;
+                padding:0px 3px;
+                border-radius: 20px;
+                /* border: 0.5px solid black; */
+                box-shadow: 0 0.5px 3px rgba(0,0,0,0.3);
+                }    
+                
+                .filter:hover {
+                    cursor: pointer;
+                }
+
+            .all{
+                background-color:dodgerblue;
+                color:white;
+            }
+
     </style>
     
     
@@ -59,7 +86,7 @@
             $xml = simplexml_load_file("../resources/products.xml") or die("Error: Cannot create object");
 
             foreach($xml->product as $product){
-                echo "<div class='card'>";
+                echo "<div class='card' data-category='{$product->type}'>";
                     echo "<img src='{$product->image}'>";
                     echo "<div class='info-card'>";
                         echo "<h4> {$product->brand} - {$product->name}</h4>";
@@ -74,9 +101,51 @@
     </div>
     
     <div class="side-section">
-        <h3>Filters<h3>
+        <h3>Filters</h3>
+        <div class="filter-section">
+                <div class="filter Cleanser"><p>Cleanser</p></div>
+                <div class="filter Toner"><p>Toner</p></div>
+                <div class="filter Serum"><p>Serum</p></div>
+                <div class="filter Moisturizer"><p>Moisturizer</p></div>
+                <div class="filter Eye-Cream"><p>Eye Cream</p></div>
+                <div class="filter Sunscreen"><p>Sunscreen</p></div>
+                <div class="filter Face-Oil"><p>Face Oil</p></div>
+                <div class="filter Exfoliator"><p>Exfoliator</p></div>
+                <div class="filter Mask"><p>Mask</p></div>
+                <div class="filter Lip-Balm"><p>Lip Balm</p></div>
+                <div class="filter Foundation"><p>Foundation</p></div>
+                <div class="filter Concealer"><p>Concealer</p></div>
+                <div class="filter Powder"><p>Powder</p></div>
+                <div class="filter Blush"><p>Blush</p></div>
+                <div class="filter Highlighter"><p>Highlighter</p></div>
+                <div class="filter Eyeshadow"><p>Eyeshadow</p></div>
+                <div class="filter Mascara"><p>Mascara</p></div>
+                <div class="filter Lipstick"><p>Lipstick</p></div>
+                <div class="filter all"><p>Reset</p></div>
+        </div>
     </div>
 </div>
+
+<script>
+    const filters = document.querySelectorAll('.filter');
+    const cards = document.querySelectorAll('.card');
+
+    filters.forEach(filter => {
+    filter.addEventListener('click', () => {
+        const category = filter.classList[1];
+        
+        cards.forEach(card => {
+        if (category === 'all' || card.dataset.category === category) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+        });
+    });
+    });
+
+
+</script>
 
 </body>
 </html>
